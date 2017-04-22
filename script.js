d1e72791e6016c923bbe4744f4625ae8d29f08e2
Kyle Hutchens <Hutch8729@gmail.com>
2017-04-22 17:50:00 -0400

    Not pretty but fully functional. not actually tested.

--- script.js
+++ script.js
@@ -4,7 +4,6 @@ var f = true;
 function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
-        // showWeather();
     } else {
         x.innerHTML = "Geolocation is not supported by this browser.";
 				
@@ -15,6 +14,7 @@ function getLocation() {
     //tester.innerHTML = "Latitude: " + position.coords.latitude + "</br>Longitude: " + position.coords.longitude;
     lat = position.coords.latitude
     lon = position.coords.longitude
+    showWeather();
  	}
 
 document.getElementById('tempType').addEventListener('click', function () {
@@ -32,45 +32,6 @@ document.getElementById('tempType').addEventListener('click', function () {
 
 $(document).ready(function(){
   getLocation();
-  // var apiurl = "https://crossorigin.me/http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=e6b16395e0d9e609c012595f72cef7a4"
-  // //$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=Toronto&APPID=e6b16395e0d9e609c012595f72cef7a4", function(a){
-  //     $.getJSON(apiurl, function(a){
-  //       var farTemp = ((a.main.temp-273.15) * (9/5) + 32).toFixed();
-  //       $('#farenheit').text(farTemp + " F");
-  //       var celTemp = (a.main.temp-273.15).toFixed(1);
-  //       $('#celsius').text(celTemp + " C");
-  //       $('#tempIcon').attr('src', 'http://openweathermap.org/img/w/' + a.weather[0].icon + '.png');
-  //       $('#locName').text("The temperature in " + a.name + " is:");
-  //       console.log(a.weather[0].id);
-  //       if(a.weather[0].id > 199 && a.weather[0].id < 300){
-  //         $(".body").css("background-color", "DimGray");
-  //       }
-  //       else if(a.weather[0].id > 299 && a.weather[0].id < 400){
-  //         $(".body").css("background-color", "DeepSkyBlue");
-  //       }
-  //       else if(a.weather[0].id > 499 && a.weather[0].id < 600){
-  //         $(".body").css("background-color", "SteelBlue");
-  //       }
-  //       else if(a.weather[0].id > 599 && a.weather[0].id < 700){
-  //         $(".body").css("background-color", "#e6e6e6");
-  //       }
-  //       else if(a.weather[0].id > 799 && a.weather[0].id < 900){
-  //         $(".body").css("background-color", "#262626");
-  //         if(a.dt > a.sys.sunrise && a.dt < a.sys.sunset){
-  //           $(".body").css("background-color", "PaleGoldenRod");
-  //         }
-  //         else {
-  //           $(".body").css("background-color", "#262626");
-  //           $('celTemp').css("color", "White");
-  //           $('farTemp').css("color", "White");
-  //           $('locName').css("color", "White");
-  //         }
-  //       }
-
-  //   		console.log(a);
-  //       console.log
-  //     }
-  //            )
 }
 )
 
